ac9ec0ff9e7b42beb89f5fcd6ab08e857884ccf8
Alexander Wei <4712707+aw31@users.noreply.github.com>
2023-06-03 21:49:49 -0700

    Much more optimization and pruning

--- differences.cc
+++ differences.cc
@@ -4,45 +4,39 @@
 #include <iostream>
 #include <vector>
 
-constexpr uint32_t P = 54;
 constexpr uint32_t Q = 625;
-constexpr uint32_t M = P * Q;
+constexpr uint32_t M = 24 * Q;
 
-std::vector<uint128_t> compute_quartic_powers(uint32_t n, bool mod_n = true) {
-  std::vector<uint128_t> quartic_powers(n);
-  for (uint128_t i = 0; i < n; i++) {
-    quartic_powers[i] = i * i * i * i;
-    if (mod_n) {
-      quartic_powers[i] %= n;
-    }
+std::vector<uint32_t> compute_quartic_powers_mod_m(uint32_t m) {
+  std::vector<uint32_t> quartic_powers(m);
+  for (uint128_t i = 0; i < m; i++) {
+    quartic_powers[i] = i * i * i * i % m;
   }
   return quartic_powers;
 }
 
-std::vector<bool> sums_of_quartic_residues_mod_m(uint32_t m) {
-  auto quartic_powers_mod_m = compute_quartic_powers(m);
-  std::vector<bool> is_sum_of_quartic_residue_mod_m(m, false);
+std::vector<uint32_t> sums_of_quartic_residues_mod_m(uint32_t m) {
+  auto quartic_powers_mod_m = compute_quartic_powers_mod_m(m);
+  std::vector<uint32_t> is_sum_of_quartic_residue_mod_m(m, 0);
   for (uint32_t i = 0; i < m; i++) {
     for (uint32_t j = 0; j < m; j++) {
       auto s = (quartic_powers_mod_m[i] + quartic_powers_mod_m[j]) % m;
-      is_sum_of_quartic_residue_mod_m[s] = true;
+      is_sum_of_quartic_residue_mod_m[s] = 1;
     }
   }
   return is_sum_of_quartic_residue_mod_m;
 }
 
 auto find_good_pairs_mod_M() {
-  auto pow4_mod_Q = compute_quartic_powers(Q);
-  auto pow4_mod_P = compute_quartic_powers(P);
-  auto is_sum_of_pow4_mod_P = sums_of_quartic_residues_mod_m(P);
-
+  auto pow4_mod_Q = compute_quartic_powers_mod_m(Q);
   std::vector<std::pair<uint32_t, uint32_t>> good_pairs;
   // Let i = d % M and j = c % M.
   for (uint32_t i = 0; i < M; i++) {
-    // MODULO 4
+    // MODULO 8
     // Filter out even numbers, since if d is even in a^4 + b^4 + c^4 = d^4,
     // then two of a, b, c must be odd, in which case LHS is 2 != 0 mod 4.
-    if (i % 2 == 0) {
+    // In fact, Ward (Duke Math. J., 1948) shows a fortiori that d = 1 mod 8.
+    if (i % 8 != 1) {
       continue;
     }
     // MODULO 5
@@ -59,10 +53,15 @@ auto find_good_pairs_mod_M() {
       if (pow4_mod_Q[i % Q] != pow4_mod_Q[j % Q]) {
         continue;
       }
-      // MODULO P
-      // Ensure (d^4 - c^4) % P is a sum of two fourth powers modulo P.
-      auto diff = (pow4_mod_P[i % P] - pow4_mod_P[j % P] + P) % P;
-      if (!is_sum_of_pow4_mod_P[diff]) {
+      // MODULO 9
+      // No sum of two fourth powers is 0 mod 9 unless both are 0 mod 3.
+      if (i % 3 == 0 && j % 3 == 0) {
+        continue;
+      }
+      // MODULO 8
+      // Ward (Duke Math. J., 1948) shows that (a % 8, b % 8, c % 8) is either a
+      // permutation of (0, 0, 1) or (0, 0, 7). Thus, j % 8 is in {0, 1, 7}.
+      if (j % 8 != 0 && j % 8 != 1 && j % 8 != 7) {
         continue;
       }
 
@@ -74,27 +73,70 @@ auto find_good_pairs_mod_M() {
 }
 
 std::vector<CandidateDifference> compute_differences(uint32_t max_d) {
-  assert(Q == 625);                  // Q is 625
-  assert(P % 2 == 0 && P % 5 != 0);  // P is even and coprime to Q
+  assert(Q == 625);           // Q is 625
+  assert(M % (24 * Q) == 0);  // 24 * Q divides M
 
   auto good_pairs = find_good_pairs_mod_M();
   std::cout << "Found " << good_pairs.size() << " good pairs ("
             << 100 * float(good_pairs.size()) / M / M << "%)" << std::endl;
 
-  // Precompute sums of quartic residues mod 2^8, 3^6, 13^2, 29^2,
+  // Precompute (sums of) quartic residues mod 2^8, 3^6, 7^3, 11^2, 13^2, 29^2
+  auto pow4_mod_256 = compute_quartic_powers_mod_m(256);
+  auto pow4_mod_729 = compute_quartic_powers_mod_m(729);
+  auto pow4_mod_343 = compute_quartic_powers_mod_m(343);
+  auto pow4_mod_121 = compute_quartic_powers_mod_m(121);
+  auto pow4_mod_169 = compute_quartic_powers_mod_m(169);
+  auto pow4_mod_841 = compute_quartic_powers_mod_m(841);
   auto is_sum_of_quartic_residues_mod_256 = sums_of_quartic_residues_mod_m(256);
   auto is_sum_of_quartic_residues_mod_729 = sums_of_quartic_residues_mod_m(729);
+  auto is_sum_of_quartic_residues_mod_343 = sums_of_quartic_residues_mod_m(343);
+  auto is_sum_of_quartic_residues_mod_121 = sums_of_quartic_residues_mod_m(121);
   auto is_sum_of_quartic_residues_mod_169 = sums_of_quartic_residues_mod_m(169);
   auto is_sum_of_quartic_residues_mod_841 = sums_of_quartic_residues_mod_m(841);
 
   // Compute quartic powers
-  auto quartic_powers = compute_quartic_powers(max_d + 1, false);
+  auto quartic_powers = std::vector<uint128_t>(max_d + 1);
+  for (uint128_t i = 0; i <= max_d; i++) {
+    quartic_powers[i] = i * i * i * i;
+  }
+
+  // MODULO P^2 for P odd, P != 1 mod 8
+  // Suppose v_P(d^4 - c^4) != 0 mod 4. Then, let a = P^m a' and b = P^m b' be
+  // such that, wlog, a' != 0 mod P. Then, v_P(a'^4 + b'^4) = v_P(d^4 - c^4) -
+  // 4m > 0, so a'^4 + b'^4 = 0 mod P. Then, b'/a' is a primitive 8th root of
+  // unity mod P, which can only happen if P = 1 mod 8.
+  //
+  // By the lifting the exponent lemma, if P does not divide either d or c, then
+  // v_P(d - c) > 0 implies v_P(d^4 - c^4) = v_P(d - c), and v_P(d + c) > 0
+  // implies v_P(d^4 - (-c)^4) = v_P(d + c). If P divides both d and c, then
+  // P divides a^4 + b^4, so P = 1 mod 8. So if P divides d - c (or d + c) not a
+  // multiple of 4 times, then P = 1 mod 8.
+  std::vector<bool> is_prime(2 * max_d + 1, true);
+  std::vector<bool> is_bad(2 * max_d + 1, false);
+  is_prime[0] = false;
+  is_prime[1] = false;
+  for (uint32_t i = 2; i <= 2 * max_d; i++) {
+    if (!is_prime[i]) {
+      continue;
+    }
+    for (uint32_t j = i + i; j <= 2 * max_d; j += i) {
+      is_prime[j] = false;
+    }
+    if (i % 8 != 1 && i != 2) {
+      for (uint32_t j = i; j <= 2 * max_d; j += i) {
+        if ((j / i) % i != 0 || (j / i / i) % i != 0 ||
+            (j / i / i / i) % i != 0) {
+          is_bad[j] = true;
+        }
+      }
+    }
+  }
 
   // Compute differences
   std::vector<CandidateDifference> differences;
-  for (int i = 0; M * i <= max_d; i++) {
-    for (int j = 0; j <= i; j++) {
-      for (auto [k, l] : good_pairs) {
+  for (auto [k, l] : good_pairs) {
+    for (int i = 0; M * i <= max_d; i++) {
+      for (int j = 0; j <= i; j++) {
         uint32_t d = M * i + k;
         uint32_t c = M * j + l;
         // Ensure 0 < c < d <= max_d
@@ -102,16 +144,44 @@ std::vector<CandidateDifference> compute_differences(uint32_t max_d) {
           continue;
         }
 
-        auto diff = quartic_powers[d] - quartic_powers[c];
-
-        // Ensure diff is a sum of two fourth powers mod 2^8, 3^6, 13^2, 29^2.
-        if (!is_sum_of_quartic_residues_mod_256[diff % 256] ||
-            !is_sum_of_quartic_residues_mod_729[diff % 729] ||
-            !is_sum_of_quartic_residues_mod_169[diff % 169] ||
-            !is_sum_of_quartic_residues_mod_841[diff % 841]) {
+        // MODULO 4096
+        // Morgan (Duke Math. J., 1948) shows that, if c is odd, d^4 - c^4 = 0
+        // mod 4096.
+        auto pow2_d_mod_4096 = (d % 4096) * (d % 4096) % 4096;
+        auto pow2_c_mod_4096 = (c % 4096) * (c % 4096) % 4096;
+        auto pow4_d_mod_4096 = pow2_d_mod_4096 * pow2_d_mod_4096 % 4096;
+        auto pow4_c_mod_4096 = pow2_c_mod_4096 * pow2_c_mod_4096 % 4096;
+        if (c % 2 == 1 && pow4_c_mod_4096 != pow4_d_mod_4096) {
+          continue;
+        }
+        // v_P(d \pm c)
+        // If v_P(d - c) != 0 mod 4 for an odd prime P != 1 mod 8 then skip.
+        // If v_P(d + c) != 0 mod 4 for an odd prime P != 1 mod 8 then skip.
+        if (is_bad[d - c] || is_bad[d + c]) {
+          continue;
+        }
+        // Ensure diff is a sum of two fourth powers mod 2^8, 3^6, 7^3, 11^2,
+        // 13^2, 29^2.
+        if (!is_sum_of_quartic_residues_mod_256
+                [(pow4_mod_256[d % 256] - pow4_mod_256[c % 256] + 256) % 256] ||
+            !is_sum_of_quartic_residues_mod_729
+                [(pow4_mod_729[d % 729] - pow4_mod_729[c % 729] + 729) % 729] ||
+            !is_sum_of_quartic_residues_mod_343
+                [(pow4_mod_343[d % 343] - pow4_mod_343[c % 343] + 343) % 343] ||
+            !is_sum_of_quartic_residues_mod_121
+                [(pow4_mod_121[d % 121] - pow4_mod_121[c % 121] + 121) % 121] ||
+            !is_sum_of_quartic_residues_mod_169
+                [(pow4_mod_169[d % 169] - pow4_mod_169[c % 169] + 169) % 169] ||
+            !is_sum_of_quartic_residues_mod_841
+                [(pow4_mod_841[d % 841] - pow4_mod_841[c % 841] + 841) % 841]) {
           continue;
         }
 
+        auto diff = quartic_powers[d] - quartic_powers[c];
+        // Check v_5(d^4 - c^4) = 0 mod 4 (noting that 625 | d^4 - c^4)
+        if (diff % 3125 == 0 && diff % 390625 != 0) {
+          continue;
+        }
         differences.push_back({diff, c, d});
       }
     }
--- main.cc
+++ main.cc
@@ -7,44 +7,57 @@
 
 typedef unsigned __int128 uint128_t;
 
-constexpr uint32_t BLOOM_FILTER_M = 1 << 28;
-std::bitset<BLOOM_FILTER_M> bloom_filter[2];
+constexpr uint32_t PREFIX_LUT_K = 26;
+constexpr uint64_t PREFIX_LUT_M = 1 << PREFIX_LUT_K;
+std::bitset<PREFIX_LUT_M> prefix_lut;
 
-constexpr uint32_t HASH_MAP_M = 1 << 25;
+constexpr uint32_t HASH_MAP_K = 22;
+constexpr uint32_t HASH_MAP_M = 1 << HASH_MAP_K;
 uint32_t hash_map[HASH_MAP_M + 16];
 
 uint32_t inline hash_1(uint64_t x) { return x >> 32; }
-uint32_t inline hash_2(uint64_t x) { return x >> 8; }
+uint32_t inline hash_2(uint64_t x) { return (x >> 24) ^ (x >> 16); }
 
 void insert(uint64_t x) {
   assert(x != 0);
-  bloom_filter[0][hash_1(x) >> 4] = true;
-  bloom_filter[1][hash_2(x) >> 4] = true;
+  prefix_lut[hash_1(x) >> (32 - PREFIX_LUT_K)] = true;
 
-  auto h = hash_1(x ^ (x << 24)) & (HASH_MAP_M - 1);
-  while (hash_map[h] != 0) {
-    h++;
+  auto i = hash_2(x) >> (32 - HASH_MAP_K);
+  while (hash_map[i] != 0) {
+    i++;
   }
   // Ensure a sentinel 0 value is left the end of the hash map. This assert will
   // fail the first time we try to write the last slot.
-  assert(h + 1 < HASH_MAP_M + 16);
-  hash_map[h] = x;
+  assert(i + 1 < HASH_MAP_M + 16);
+  hash_map[i] = x;
 }
 
 bool contains(uint64_t x) {
-  if (bloom_filter[0][hash_1(x) >> 4] && bloom_filter[1][hash_2(x) >> 4]) {
+  if (prefix_lut[hash_1(x) >> (32 - PREFIX_LUT_K)]) {
     auto x_int32 = uint32_t(x);
-    auto h = hash_1(x ^ (x << 24)) & (HASH_MAP_M - 1);
-    while (hash_map[h] != 0 && hash_map[h] != x_int32) {
-      h++;
+    auto i = hash_2(x) >> (32 - HASH_MAP_K);
+    while (hash_map[i] != 0 && hash_map[i] != x_int32) {
+      i++;
     }
-    return hash_map[h] != 0;
+    return hash_map[i] != 0;
   }
   return false;
 }
 
 constexpr uint32_t MAX_D = 500000;
-uint64_t quartic_powers_uint64[MAX_D + 1];
+uint64_t pow4_uint64[MAX_D + 1];
+
+void verify_ab(std::vector<CandidateDifference> candidate_differences,
+               uint32_t a, uint32_t b) {
+  auto sum = uint128_t(1) * a * a * a * a + uint128_t(1) * b * b * b * b;
+  for (auto [diff, c, d] : candidate_differences) {
+    if (diff == sum) {
+      std::cout << std::endl;
+      std::cout << "Solution found: " << a << "^4 + " << b << "^4 + " << c
+                << "^4 = " << d << "^4" << std::endl;
+    }
+  }
+}
 
 int main() {
   Timer t = Timer();
@@ -58,37 +71,35 @@ int main() {
   for (auto [diff, _c, _d] : differences) {
     insert(diff / 625);
   }
-  t.log_task("Populate Bloom filter and hash map");
+  t.log_task("Populate filter and hash map");
 
   // Precompute quartic powers
   for (uint128_t i = 0; i <= MAX_D; i++) {
-    quartic_powers_uint64[i] = i * i * i * i;
+    pow4_uint64[i] = i * i * i * i;
   }
 
   // Check all pairwise sums, assuming wlog that a = 5 * i and b = 5 * j
-  for (uint32_t i = 1; i <= MAX_D / 5; i++) {
-    for (uint32_t j = 1; j <= i; j++) {
-      // MODULO 4
-      // If a and b are both odd, then a^4 + b^4 = 2 mod 4. However, no
-      // difference of fourth powers is 2 mod 4.
-      if (i % 2 == 1 && j % 2 == 1) {
-        continue;
+  // Ward (Duke Math. J., 1948) shows that (a % 8, b % 8, c % 8) is a
+  // permutation of either (0, 0, 1) or (0, 0, 7). Therefore, (i % 8, j % 8) =
+  // (0, 0), (5, 0), (0, 5), (3, 0), or (0, 3).
+  for (uint32_t i = 8; i <= MAX_D / 5; i += 8) {
+    for (uint32_t j = 8; j <= i; j += 8) {
+      if (contains(pow4_uint64[i] + pow4_uint64[j])) [[unlikely]] {
+        verify_ab(differences, 5 * i, 5 * j);
       }
-      auto sum_uint64 = quartic_powers_uint64[i] + quartic_powers_uint64[j];
-      if (contains(sum_uint64)) [[unlikely]] {
-        uint32_t a = 5 * i;
-        uint32_t b = 5 * j;
-        auto sum = uint128_t(1) * a * a * a * a + uint128_t(1) * b * b * b * b;
-        for (auto [diff, c, d] : differences) {
-          if (diff == sum) {
-            t.log_task("Check pairwise sums");
-            std::cout << std::endl;
-            std::cout << "Solution found: " << a << "^4 + " << b << "^4 + " << c
-                      << "^4 = " << d << "^4" << std::endl;
-            return 0;
-          }
-        }
+      if (contains(pow4_uint64[i + 5] + pow4_uint64[j])) [[unlikely]] {
+        verify_ab(differences, 5 * (i + 5), 5 * j);
+      }
+      if (contains(pow4_uint64[i] + pow4_uint64[j + 5])) [[unlikely]] {
+        verify_ab(differences, 5 * i, 5 * (j + 5));
+      }
+      if (contains(pow4_uint64[i + 3] + pow4_uint64[j])) [[unlikely]] {
+        verify_ab(differences, 5 * (i + 3), 5 * j);
+      }
+      if (contains(pow4_uint64[i] + pow4_uint64[j + 3])) [[unlikely]] {
+        verify_ab(differences, 5 * i, 5 * (j + 3));
       }
     }
   }
+  t.log_task("Check pairwise sums");
 }
